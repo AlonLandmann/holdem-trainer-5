fc2b9df9419d23250900aab1cb3177dc9792fad8
AlonLandmann <rafael.landmann@gmail.com>
2024-08-27 19:17:50 +0800

    adjust global font-color

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang='en'>
       <Head />
-      <body className='dark bg-neutral-950'>
+      <body className='dark bg-neutral-950 text-neutral-200'>
         <Main />
         <NextScript />
       </body>
