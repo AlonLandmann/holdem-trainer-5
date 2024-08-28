644198c3fd78d4455d42bda93bfc7b729af11743
AlonLandmann <rafael.landmann@gmail.com>
2024-08-28 11:06:58 +0800

    choose fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Red Hat Display', 'sans-serif'],
+        decorative: ['Amita', 'cursive'],
+      },
+    },
   },
   plugins: [],
 }
