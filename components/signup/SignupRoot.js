4c7c62d9bee34083de26c1e4b20a14ece10067e8
AlonLandmann <rafael.landmann@gmail.com>
2024-08-28 14:19:35 +0800

    adjust sign up form styling

--- components/signup/SignupRoot.js
+++ components/signup/SignupRoot.js
@@ -10,14 +10,14 @@ export default function SignupRoot() {
 
   return (
     <div className='min-h-screen p-3 flex justify-center items-center'>
-      <form className='max-w-96 pt-12 pb-7 px-9 bg-neutral-900 rounded flex flex-col items-center'>
+      <form className='max-w-96 pt-12 pb-7 px-11 bg-neutral-900 rounded flex flex-col items-center'>
         <h1 className='font-decorative text-5xl text-neutral-700 mb-9'>
           HT
         </h1>
-        <h3 className='font-medium text-2xl mb-4'>
+        <h3 className='font-medium text-2xl mb-7'>
           Hello
         </h3>
-        <p className='text-neutral-500 mb-5'>
+        <p className='text-neutral-500 mb-4'>
           Sign up using your email
         </p>
         <div className='flex flex-col gap-3 w-full pb-5'>
@@ -50,7 +50,9 @@ export default function SignupRoot() {
         </div>
         <div className='flex items-center self-stretch gap-2 px-2'>
           <div className='flex-grow bg-neutral-600 h-[1px]'></div>
-          <div className='text-xs text-neutral-400 tracking-wider'>OR</div>
+          <div className='text-xs text-neutral-400 tracking-wider'>
+            OR
+          </div>
           <div className='flex-grow bg-neutral-600 h-[1px]'></div>
         </div>
         <div className='pt-5 pb-7 self-stretch'>
