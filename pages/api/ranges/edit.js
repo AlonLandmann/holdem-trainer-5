7b37f71f5e02a0070318c9d4c01dec3e9b9ffbcd
AlonLandmann <rafael.landmann@gmail.com>
2024-09-02 14:20:03 +0800

    add edit-range route

---
+++ pages/api/ranges/edit.js
@@ -0,0 +1,25 @@
+import { toStorageFormat } from '@/lib/server/ranges'
+import handleUserRequest from '@/lib/server/routes'
+
+export default async function handler(req, res) {
+  return handleUserRequest(req, res, 'PUT', async (prisma, user) => {
+    const updatedRange = toStorageFormat(req.body)
+
+    await prisma.range.update({
+      where: {
+        id: updatedRange.id
+      },
+      data: {
+        stacks: updatedRange.stacks,
+        history: updatedRange.history,
+        options: updatedRange.options,
+        matrix: updatedRange.matrix,
+        predecessor: updatedRange.predecessorId
+          ? { connect: { id: updatedRange.predecessorId } }
+          : null
+      }
+    })
+
+    return res.status(200).json({ success: true })
+  })
+}
