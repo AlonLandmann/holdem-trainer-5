8e5963f11d12778da5e1bf49619b6244d2b5a14e
AlonLandmann <rafael.landmann@gmail.com>
2024-09-06 13:00:48 +0800

    extract trainer toolbar

---
+++ components/app/trainer/Toolbar.js
@@ -0,0 +1,25 @@
+import Button from '@/components/_common_/Button'
+
+export default function Toolbar({ setSidebarInView, setStatsInView }) {
+  return (
+    <div className='p-3 flex items-center border-b'>
+      <h1 className='text-neutral-600'>
+        Trainer
+      </h1>
+      <div className='flex gap-2 ml-auto'>
+        <Button
+          theme='tertiary'
+          utilClasses='w-8 h-8 flex justify-center items-center'
+          icon='ui-checks'
+          onClick={() => { setSidebarInView(prev => !prev) }}
+        />
+        <Button
+          theme='tertiary'
+          utilClasses='w-8 h-8 flex justify-center items-center'
+          icon='graph-up-arrow'
+          onClick={() => { setStatsInView(prev => !prev) }}
+        />
+      </div>
+    </div>
+  )
+}
--- components/app/trainer/TrainerMain.js
+++ components/app/trainer/TrainerMain.js
@@ -1,5 +1,5 @@
-import Button from '@/components/_common_/Button'
 import { useState } from 'react'
+import Toolbar from './Toolbar'
 
 export default function TrainerMain() {
   const [sidebarInView, setSidebarInView] = useState(true)
@@ -7,25 +7,10 @@ export default function TrainerMain() {
 
   return (
     <div className='grow bg-neutral-900'>
-      <div className='p-3 flex items-center border-b'>
-        <h1 className='text-neutral-600'>
-          Trainer
-        </h1>
-        <div className='flex gap-2 ml-auto'>
-          <Button
-            theme='tertiary'
-            utilClasses='w-8 h-8 flex justify-center items-center'
-            icon='ui-checks'
-            onClick={() => { setSidebarInView(prev => !prev) }}
-          />
-          <Button
-            theme='tertiary'
-            utilClasses='w-8 h-8 flex justify-center items-center'
-            icon='graph-up-arrow'
-            onClick={() => { setStatsInView(prev => !prev) }}
-          />
-        </div>
-      </div>
+      <Toolbar
+        setSidebarInView={setSidebarInView}
+        setStatsInView={setStatsInView}
+      />
     </div>
   )
 }
