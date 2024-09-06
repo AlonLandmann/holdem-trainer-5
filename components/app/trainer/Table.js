3b378203a5f65245e74fd6d3b39596858ab074a1
AlonLandmann <rafael.landmann@gmail.com>
2024-09-06 17:52:14 +0800

    add chips in the middle

--- components/app/trainer/Table.js
+++ components/app/trainer/Table.js
@@ -9,11 +9,12 @@ export default function Table({ spot, holeCards, heroPosition, flash }) {
         ${flash === 'correct' ? 'border-[#66c24a]' : ''}
         ${flash === 'incorrect' ? 'border-[#c43333]' : ''}
       `}>
-        <div className='absolute z-10 rounded-[3px] p-1 flex gap-1 bg-[#11111188]'>
-          {spot.board.map((card, i) => (
-            <div
-              key={'card' + i}
-              className={`
+        {spot.board.length > 0 &&
+          <div className='absolute z-10 rounded-[3px] p-1 flex gap-1 bg-[#11111188]'>
+            {spot.board.map((card, i) => (
+              <div
+                key={'card' + i}
+                className={`
                 relative h-[50px] w-[33px] rounded-[3px] flex justify-center
                 items-center text-neutral-400 text-lg overflow-hidden
                 ${card[1] === 's' ? 'bg-[#151515]' : ''}
@@ -21,14 +22,19 @@ export default function Table({ spot, holeCards, heroPosition, flash }) {
                 ${card[1] === 'd' ? 'bg-[#2e6067]' : ''}
                 ${card[1] === 'c' ? 'bg-[#3a6e48]' : ''}
               `}
-            >
-              {card[0]}
-              <div className='absolute top-4 left-1 text-5xl opacity-5'>
-                <i className={suitIcon(card[1])}></i>
+              >
+                {card[0]}
+                <div className='absolute top-4 left-1 text-5xl opacity-5'>
+                  <i className={suitIcon(card[1])}></i>
+                </div>
               </div>
+            ))}
+            <div className='absolute top-full mt-1 left-1/2 -translate-x-1/2 text-sm flex items-center gap-2 text-neutral-500'>
+              <i className='bi bi-database'></i>
+              <div>{spot.pot - spot.committedAtRound.reduce((a, c) => a + c, 0)}</div>
             </div>
-          ))}
-        </div>
+          </div>
+        }
         <div className='relative h-[290px] w-[580px] rounded-[145px] flex justify-center items-center text-[100px] bg-[#141414]'>
           <div className='font-decorative text-neutral-800 select-none text-[100px]'>
             HT
