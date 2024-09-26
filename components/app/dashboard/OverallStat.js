739dbf21b26d006c9fbd4ab426949a7d9656f87c
AlonLandmann <rafael.landmann@gmail.com>
2024-09-26 20:34:59 +0800

    add total combos trained

---
+++ components/app/dashboard/OverallStat.js
@@ -0,0 +1,13 @@
+export default function OverallStat({ icon, number, label }) {
+  return (
+    <div className='relative flex flex-col justify-center items-center px-5 py-3 border rounded w-52 h-28 gap-2'>
+      <i className={`bi bi-${icon} text-neutral-700 text-xl absolute top-1 right-2`}></i>
+      <h1 className='text-3xl'>
+        {number}
+      </h1>
+      <label className='text-neutral-500 uppercase tracking-wide text-sm'>
+        {label}
+      </label>
+    </div>
+  )
+}
--- components/app/dashboard/OverallStats.js
+++ components/app/dashboard/OverallStats.js
@@ -1,7 +1,24 @@
+import { totalCombos } from '@/lib/client/stats'
+import OverallStat from './OverallStat'
+
 export default function OverallStats({ user }) {
   return (
-    <div>
-      
-    </div>
+    <section className='flex p-12 gap-6'>
+      <OverallStat
+        icon='crosshair'
+        number={totalCombos(user.trainingSessions)}
+        label='combos trained'
+      />
+      <OverallStat
+        icon='columns-gap'
+        number={totalCombos(user.trainingSessions)}
+        label='avg complexity'
+      />
+      <OverallStat
+        icon='graph-up-arrow'
+        number={totalCombos(user.trainingSessions)}
+        label='total score'
+      />
+    </section>
   )
 }
