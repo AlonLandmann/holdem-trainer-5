5dee02865e4f872b7a7ec938286edf86a69240bd
AlonLandmann <rafael.landmann@gmail.com>
2024-09-26 22:03:21 +0800

    add nr of ranges

--- components/app/dashboard/OverallStats.js
+++ components/app/dashboard/OverallStats.js
@@ -5,6 +5,11 @@ export default function OverallStats({ user }) {
   return (
     <section className='flex p-12 gap-6'>
       <OverallStat
+        icon='ui-checks'
+        number={user.nrRanges}
+        label='number of ranges'
+      />
+      <OverallStat
         icon='crosshair'
         number={totalCombos(user.trainingSessions)}
         label='combos trained'
