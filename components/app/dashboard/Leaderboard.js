52dbb74f329308802a8b1dbed0ad300b52d4e45f
AlonLandmann <rafael.landmann@gmail.com>
2024-09-28 03:13:01 +0800

    highlight current position on the leaderboard

--- components/app/dashboard/Leaderboard.js
+++ components/app/dashboard/Leaderboard.js
@@ -23,9 +23,15 @@ export default function Leaderboard({ user }) {
       <h3 className='px-2 text-neutral-500 text-lg mb-2'>
         Leaderboard
       </h3>
-      <div className='p-4 flex flex-col gap-2 border rounded'>
+      <div className='p-3 flex flex-col gap-1 border rounded'>
         {leaderboard.slice(0, 10).map((rank, i) => (
-          <div key={'rank' + i} className='flex items-center gap-2 text-sm'>
+          <div
+            key={'rank' + i}
+            className={`
+              flex items-center gap-2 text-sm py-1 px-3
+              ${user.id === rank.id ? 'bg-neutral-800  rounded-sm' : ''}
+            `}
+          >
             <div className='font-mono min-w-4 text-neutral-500'>
               {i + 1}.
             </div>
@@ -35,7 +41,7 @@ export default function Leaderboard({ user }) {
             <div className='font-mono min-w-36 text-right ml-auto'>
               {rank.totalScore.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
             </div>
-            <div className='min-w-16 flex justify-start ml-2 text-xs'>
+            <div className='min-w-16 flex justify-start ml-2 text-xs grayscale opacity-75'>
               <RankBanner
                 rank={getMilitaryRank(rank.totalScore)}
               />
