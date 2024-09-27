7d1bd3ffcd35c6561ff41f88e1ab55c7a9d512be
AlonLandmann <rafael.landmann@gmail.com>
2024-09-28 04:34:01 +0800

    adjust leaderboard

--- components/app/dashboard/Leaderboard.js
+++ components/app/dashboard/Leaderboard.js
@@ -41,7 +41,7 @@ export default function Leaderboard({ user }) {
             <div className='font-mono min-w-36 text-right ml-auto'>
               {rank.totalScore.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
             </div>
-            <div className='min-w-16 flex justify-start ml-2 text-xs grayscale opacity-75'>
+            <div className={`min-w-16 flex justify-start ml-2 text-xs ${user.id === rank.id ? '' : 'opacity-25'}`}>
               <RankBanner
                 rank={getMilitaryRank(rank.totalScore)}
               />
