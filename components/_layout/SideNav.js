192eb49da34ad17d443704c44a15e3ed2fb0ba1d
AlonLandmann <rafael.landmann@gmail.com>
2024-10-07 12:54:40 +0800

    make sidenav smooth on reloads

--- components/_layout/SideNav.js
+++ components/_layout/SideNav.js
@@ -1,33 +1,40 @@
 import SideNavItem from '@/components/_layout/SideNavItem'
 import Button from '@/components/_ui/Button'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import toast from 'react-hot-toast'
 import A from '../_ui/A'
 
 export default function SideNav() {
   const [extendedView, setExtendedView] = useState(false)
-  
+  const [listenForEnter, setListenForEnter] = useState(false)
+
+  useEffect(() => {
+    const timer = setTimeout(() => { setListenForEnter(true) }, 200)
+    return () => clearTimeout(timer)
+  }, [])
+
   async function handleLogout() {
     const res = await fetch('/api/auth/logout', { method: 'POST', credentials: 'include' })
     const json = await res.json()
-    
+
     if (json.success) {
       window.location = '/auth/login'
     } else {
       toast.error(json.message || 'An unexpected error occurred.')
     }
   }
-  
+
   const groupStyle = 'flex flex-col gap-2 px-2 py-2'
   const separatorStyle = `h-[1px] ${extendedView ? 'w-32' : 'w-7'} bg-neutral-800 self-center my-2`
 
   return (
     <>
-      <div className={`${extendedView ? 'min-w-14' : 'hidden'}`}></div>
+      <div className='min-w-14'></div>
       <div
-        className={`h-screen ${extendedView ? 'w-40 absolute z-20' : 'w-14'} flex flex-col bg-neutral-900 border-r`}
-        onMouseEnter={() => { setExtendedView(true) }}
+        className={`h-screen absolute z-20 ${extendedView ? 'w-40' : 'w-14'} flex flex-col bg-neutral-900 border-r`}
+        onMouseEnter={() => { if (listenForEnter) { setExtendedView(true) } }}
         onMouseLeave={() => { setExtendedView(false) }}
+        style={{ transition: 'width 200ms' }}
       >
         <div className={groupStyle}>
           <A
@@ -61,7 +68,7 @@ export default function SideNav() {
             text='Trainer'
             href='/app/trainer'
             extendedView={extendedView}
-            />
+          />
           <SideNavItem
             icon='book'
             text='Academy'
