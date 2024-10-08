9dc7e9d3a0571714dd2cb95aa46feed2a7327518
AlonLandmann <rafael.landmann@gmail.com>
2024-10-08 13:58:08 +0800

    ensure unique usernames

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -2,6 +2,7 @@ import { setSessionCookie } from '@/lib/cookies'
 import { sendVerificationLink } from '@/lib/email'
 import prisma from '@/lib/prisma'
 import { generateUsername } from '@/lib/usernames'
+import { last } from 'lodash'
 import sha256 from 'sha256'
 
 export default async function handler(req, res) {
@@ -14,10 +15,29 @@ export default async function handler(req, res) {
           return res.status(200).json({ success: false, message: 'User already exists.' })
         }
 
+        let userNameCandidate = generateUsername()
+
+        const conflictingUsers = await prisma.user.findMany({
+          where: {
+            username: {
+              startsWith: userNameCandidate
+            }
+          },
+          select: {
+            username: true
+          }
+        })
+
+        const conflictingEndings = conflictingUsers.map(u => Number(u.username.slice(userNameCandidate.length)))
+        
+        if (conflictingEndings.length) {
+          userNameCandidate = `${userNameCandidate} ${String(last(conflictingEndings) + 1)}`
+        }
+
         const newUser = await prisma.user.create({
           data: {
             email,
-            username: generateUsername(),
+            username: userNameCandidate,
             hash: sha256(password),
             session: { create: {} },
             settings: { create: {} },
