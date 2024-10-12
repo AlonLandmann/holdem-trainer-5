e74bde7aaecbccaf133e025545672e6934e9288c
AlonLandmann <rafael.landmann@gmail.com>
2024-10-12 13:04:38 +0800

    remove editor sidebar component

--- components/editor/EditorMain.js
+++ components/editor/EditorMain.js
@@ -1,7 +1,10 @@
+import Brush from '@/components/editor/Brush'
 import Categories from '@/components/editor/Categories'
+import History from '@/components/editor/History'
 import Legend from '@/components/editor/Legend'
 import Matrix from '@/components/editor/Matrix'
-import Sidebar from '@/components/editor/Sidebar'
+import Predecessor from '@/components/editor/Predecessor'
+import Stacks from '@/components/editor/Stacks'
 import Toolbar from '@/components/editor/Toolbar'
 import { useRouter } from 'next/router'
 import { useState } from 'react'
@@ -38,15 +41,32 @@ export default function EditorMain({ user, setViewHotkeyInfo }) {
         setViewHotkeyInfo={setViewHotkeyInfo}
       />
       <div className='flex p-3 gap-3'>
-        <Sidebar
-          range={range}
-          setRange={setRangeWithUndo}
-          selected={selected}
-          setSelected={setSelected}
-          setOptionHover={setOptionHover}
-          error={error}
-          setError={setError}
-        />
+        <div
+          className='flex flex-col gap-3 overflow-y-auto no-scrollbar'
+          style={{ maxHeight: 'calc(100vh - 49px - 24px)' }}
+        >
+          <Stacks
+            range={range}
+            setRange={setRangeWithUndo}
+            setError={setError}
+          />
+          <History
+            range={range}
+            setRange={setRangeWithUndo}
+            error={error}
+          />
+          <Predecessor
+            range={range}
+            setRange={setRangeWithUndo}
+          />
+          <Brush
+            range={range}
+            setRange={setRangeWithUndo}
+            selected={selected}
+            setSelected={setSelected}
+            setOptionHover={setOptionHover}
+          />
+        </div>
         <Matrix
           range={range}
           selected={selected}
