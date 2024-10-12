e6ebe6d2f03d04ef8d22df81da7104b9c96a4fa0
AlonLandmann <rafael.landmann@gmail.com>
2024-10-12 21:04:03 +0800

    adjust trainer start page

--- components/trainer2/StartMain.js
+++ components/trainer2/StartMain.js
@@ -12,31 +12,27 @@ export default function StartMain({ user, selected, setSelected }) {
   }
 
   return (
-    <div className='p-3'>
-      <h1>
-        Select ranges to train
-      </h1>
-      <div className='grid grid-cols-4 gap-x-5 gap-y-8'>
-        {user.folders.map(folder => (
-          <div key={'folder' + folder.id}>
-            <h3>
-              {folder.name}
-            </h3>
-            <div className='flex flex-col gap-1'>
-              {folder.ranges.map(range => (
-                <div key={'range' + range.id} className='flex items-center gap-2'>
-                  <Checkbox
-                    isSelected={selected.includes(range.id)}
-                    onClick={() => { handleToggle(range.id) }}
-                  />
-                  <div>{range.name}</div>
-                  <div>{range.complexity.toFixed(2)}</div>
+    <div className='p-3 grid grid-cols-4 gap-x-5 gap-y-8'>
+      {user.folders.map(folder => (
+        <div key={'folder' + folder.id}>
+          <h3 className='text-neutral-400 bg-neutral-800 py-2 px-3 rounded-sm mb-1 bg-opacity-40'>
+            {folder.name}
+          </h3>
+          <div className='flex flex-col gap-1 p-1'>
+            {folder.ranges.map(range => (
+              <div key={'range' + range.id} className='flex items-center gap-2'>
+                <Checkbox
+                  isSelected={selected.includes(range.id)}
+                  onClick={() => { handleToggle(range.id) }}
+                />
+                <div className='text-neutral-300 text-sm'>
+                  {range.name}
                 </div>
-              ))}
-            </div>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
     </div>
   )
 }
