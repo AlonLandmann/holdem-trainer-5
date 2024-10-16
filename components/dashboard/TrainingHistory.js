bb62b1c73147cdb0672505c73db08337958313b7
AlonLandmann <rafael.landmann@gmail.com>
2024-10-16 20:30:04 +0800

    adjust training history chart

--- components/dashboard/TrainingHistory.js
+++ components/dashboard/TrainingHistory.js
@@ -1,15 +1,8 @@
-import { useEffect, useState } from 'react'
-
-function formatDate(date) {
-  const native = new Date(date)
-  const year = native.getFullYear()
-  const month = String(native.getMonth() + 1).padStart(2, '0')
-  const day = String(native.getDate()).padStart(2, '0')
-  return `${year}-${month}-${day}`
-}
+import { useEffect, useRef, useState } from 'react'
 
 export default function TrainingHistory({ user }) {
   const [trainingHistory, setTrainingHistory] = useState(null)
+  const containerRef = useRef(null)
 
   useEffect(() => {
     (async () => {
@@ -25,38 +18,39 @@ export default function TrainingHistory({ user }) {
     })()
   }, [])
 
+  useEffect(() => {
+    if (containerRef.current) {
+      containerRef.current.scrollLeft = containerRef.current.scrollWidth
+    }
+  }, [trainingHistory])
+
   const today = new Date()
   const startCandidate1 = new Date(today.getFullYear(), today.getMonth() - 3, 1)
   const startCandidate2 = (trainingHistory && trainingHistory[0]) ? new Date(trainingHistory[0].date) : today
   const start = startCandidate1.getTime() > startCandidate2.getTime() ? startCandidate1 : startCandidate2
-  const end = new Date(today.getFullYear(), today.getMonth() + 1, 0)
+  const end = today
   const diffInDays = (end - start) / 1000 / 3600 / 24
   const span = []
-  const monthNames = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
-  const months = []
+  const dailyMax = trainingHistory && trainingHistory.reduce((acc, curr) => Math.max(acc, curr.total), 0)
 
   if (trainingHistory) {
     for (let i = 0; i < diffInDays; i++) {
       const dt = new Date(start.getFullYear(), start.getMonth(), start.getDate() + i)
-
       span.push(formatDate((dt)))
-
-      if (dt.getDate() === 1) {
-        months.push({ name: monthNames[dt.getMonth()], days: 1 })
-      } else {
-        if (months.length) {
-          months[months.length - 1].days++
-        }
-      }
     }
   }
 
-  function dailyMax() {
-    return trainingHistory.reduce((acc, curr) => Math.max(acc, curr.total), 0)
+  function formatDate(date) {
+    const native = new Date(date)
+    const year = native.getFullYear()
+    const month = String(native.getMonth() + 1).padStart(2, '0')
+    const day = String(native.getDate()).padStart(2, '0')
+    return `${year}-${month}-${day}`
   }
 
+
   function percentageHeight(num) {
-    return `${(100 * num / dailyMax()).toFixed(0)}%`
+    return `${(100 * num / dailyMax).toFixed(0)}%`
   }
 
   function getNum(day) {
@@ -69,35 +63,36 @@ export default function TrainingHistory({ user }) {
 
   function tallestLine() {
     let scale = 1
-    const max = dailyMax()
 
-    if (max > 100) { scale = 10 }
-    if (max > 500) { scale = 50 }
-    if (max > 1000) { scale = 100 }
-    if (max > 5000) { scale = 500 }
-    if (max > 10000) { scale = 1000 }
+    if (dailyMax > 100) { scale = 10 }
+    if (dailyMax > 500) { scale = 50 }
+    if (dailyMax > 1000) { scale = 100 }
+    if (dailyMax > 5000) { scale = 500 }
+    if (dailyMax > 10000) { scale = 1000 }
 
-    return Math.floor(dailyMax() / scale) * scale
+    return Math.floor(dailyMax / scale) * scale
   }
 
   return !trainingHistory ? null : (
-    <div className='mb-10 pr-5 self-stretch'>
-      <div className='pl-20 pb-5 pt-12'>
-        <div className='relative flex gap-[2px] items-end h-[200px]'>
-          {span.map(day => (
-            <div
-              key={day}
-              className='bg-neutral-400 w-1 z-10 opacity-80'
-              style={{ height: percentageHeight(getNum(day)) }}
-            >
+    <div className='mb-14 self-stretch flex justify-center'>
+      <div className='pl-20 pr-5 pt-12 max-w-full'>
+        <div className='relative'>
+          <div className='flex gap-[2px] items-end h-[200px] overflow-x-auto' ref={containerRef}>
+            {span.map(day => (
+              <div
+                key={day}
+                className='bg-neutral-600 z-10 min-w-2 opacity-80'
+                style={{ height: percentageHeight(getNum(day)) }}
+              >
 
-            </div>
-          ))}
+              </div>
+            ))}
+          </div>
           {Array(5).fill('').map((_, i) => (
             <div
               key={'grid-line' + i}
               className='absolute bottom-0 left-0 w-full h-[1px] bg-neutral-800 z-0'
-              style={{ bottom: `${((tallestLine() / dailyMax()) * 200 * i * 0.25)}px` }}
+              style={{ bottom: `${((tallestLine() / dailyMax) * 200 * i * 0.25)}px` }}
             >
 
             </div>
@@ -106,21 +101,10 @@ export default function TrainingHistory({ user }) {
             <div
               key={'grid-line' + i}
               className='absolute bottom-0 -left-16 z-0 text-right w-12 text-sm font-mono text-neutral-500'
-              style={{ bottom: `${((tallestLine() / dailyMax()) * 200 * i * 0.25) - 8}px` }}
+              style={{ bottom: `${((tallestLine() / dailyMax) * 200 * i * 0.25) - 8}px` }}
             >
               {(tallestLine() * 0.25 * i).toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
             </div>
-          ))}
-        </div>
-        <div className='flex'>
-          {months.map((month, i) => (
-            <label
-              key={month.name}
-              className={`py-1 text-neutral-500 text-center ${i ? 'border-l pl-[2px]' : ''}`}
-              style={{ width: `${6 * month.days}px` }}
-            >
-              {month.name}
-            </label>
           ))}
         </div>
       </div>
