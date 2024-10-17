4e5d2da3d3f3ced5b0dfb955ba088a5f73e639ea
AlonLandmann <rafael.landmann@gmail.com>
2024-10-17 17:56:17 +0800

    secure article-editor page somewhat

--- components/article-editor/ArticleEditorRoot.js
+++ components/article-editor/ArticleEditorRoot.js
@@ -1,11 +1,13 @@
 import Article from '@/components/article/Article'
 import ArticleEditor from '@/components/article-editor/ArticleEditor'
 import { useState } from 'react'
+import { useUser } from '@/hooks/useUser'
 
 export default function ArticleEditorRoot({ article, suggestions, authors }) {
+  const [user, setUser] = useUser()
   const [copy, setCopy] = useState(article)
 
-  return !copy ? null : (
+  return !copy || !user || user.role !== 'admin' ? null : (
     <div className='grid grid-cols-2'>
       <ArticleEditor
         copy={copy}
