72a3681d0c5fbc0dfc151eb90126199e8d3e9457
AlonLandmann <rafael.landmann@gmail.com>
2024-10-22 10:33:40 +0800

    add hover to demo matrix

--- components/home/Hero.js
+++ components/home/Hero.js
@@ -1,12 +1,13 @@
+import { useState } from 'react'
 import Button from '../_ui/Button'
 import Matrix from '../editor/Matrix'
 import Table from '../trainer/Table'
 
 export default function Hero({ range }) {
+  const [hovered, setHovered] = useState([])
+  
   return (
-    <section
-      className='flex flex-wrap justify-center gap-10 p-10'
-    >
+    <section className='flex flex-wrap justify-center gap-10 p-10'>
       <div className='flex flex-col justify-around'>
         <div className='max-w-[50ch] flex flex-col text-start z-20'>
           <h1 className='mb-5 m-w-[500px] text-3xl font-medium text-neutral-50 md:text-4xl'>
@@ -15,7 +16,7 @@ export default function Hero({ range }) {
           <h3 className='mb-7 max-w-[500px] text-neutral-300 text-sm md:text-base'>
             Create your own custom poker ranges and train them on Hold'em Trainer for free.
           </h3>
-          <div className='flex gap-5'>
+          <div className='flex gap-3'>
             <Button
               theme='nice'
               utilClasses='rounded-sm py-3 px-4 bg-opacity-80'
@@ -41,8 +42,8 @@ export default function Hero({ range }) {
         range={range}
         selected={[]}
         setSelected={() => { }}
-        hovered={[]}
-        setHovered={() => { }}
+        hovered={hovered}
+        setHovered={setHovered}
         optionHover={null}
       />
     </section>
