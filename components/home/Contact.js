f259f42cc91b7fda745c423893506006d3244d6b
AlonLandmann <rafael.landmann@gmail.com>
2024-10-24 14:48:23 +0800

    adjust contact

--- components/home/Contact.js
+++ components/home/Contact.js
@@ -6,19 +6,19 @@ export default function Contact() {
       <h1 className='text-4xl mb-2'>
         Reach out
       </h1>
-      <h2 className='text-lg text-neutral-500 mb-16'>
+      <h2 className='text-lg text-neutral-500 mb-8'>
         We are always pleased to here from you.
       </h2>
-      <div className='flex gap-16 text-6xl mb-16'>
+      <A
+        href='mailto:info@holdem-trainer.com'
+        text='info@holdem-trainer.com'
+        utilClasses='text-xl mb-16'
+      />
+      <div className='flex gap-16 text-6xl'>
         <A href='/' icon='youtube' />
         <A href='/' icon='twitter-x' />
         <A href='/' icon='discord' />
       </div>
-      <A
-        href='mailto:info@holdem-trainer.com'
-        text='info@holdem-trainer.com'
-        utilClasses='text-xl'
-      />
     </section>
   )
 }
