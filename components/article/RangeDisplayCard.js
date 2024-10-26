71e4150198f89222c831aa1cfc088b135af6954e
AlonLandmann <rafael.landmann@gmail.com>
2024-10-26 10:40:56 +0800

    duplicate from articles

--- components/article/RangeDisplayCard.js
+++ components/article/RangeDisplayCard.js
@@ -2,10 +2,12 @@ import Button from '@/components/_ui/Button'
 import MatrixDisplay from '@/components/manager/MatrixDisplay'
 import RangeHistory from '@/components/manager/RangeHistory'
 import RangeLegend from '@/components/manager/RangeLegend'
+import { useUser } from '@/hooks/useUser'
 import { useEffect, useState } from 'react'
 import toast from 'react-hot-toast'
 
 export default function RangeDisplayCard({ rangeId }) {
+  const [user, setUser] = useUser()
   const [range, setRange] = useState(null)
 
   useEffect(() => {
@@ -22,7 +24,23 @@ export default function RangeDisplayCard({ rangeId }) {
   }, [rangeId])
 
   async function handleDuplicate() {
+    if (!user) {
+      window.location = '/auth/login'
+    } else if (range) {
+      const res = await fetch('/api/ranges/duplicate-from-article', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(range)
+      })
 
+      const json = await res.json()
+
+      if (json.success) {
+        toast.success('Range has been added to your repertoire.')
+      } else {
+        toast.error(json.message || 'An unexpected error occurred.')
+      }
+    }
   }
 
   return !range ? <div>loading....</div> : (
