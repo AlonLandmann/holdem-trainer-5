6bc0e7155bc928cfd8ab19616c5d3d259eb939a7
AlonLandmann <rafael.landmann@gmail.com>
2024-10-29 17:34:36 +0800

    adjust window dimension hook

--- hooks/useWindowDimensions.js
+++ hooks/useWindowDimensions.js
@@ -1,17 +1,17 @@
 import { useState, useEffect } from 'react'
 
 export default function useWindowDimensions() {
-  const [dimensions, setDimensions] = useState({ w: 0, h: 0 })
+  const [dimensions, setDimensions] = useState([0, 0])
 
   useEffect(() => {
     function handleResize() {
-      setWindowDims({ w: window.innerWidth, h: window.innerHeight })
+      setDimensions([window.innerWidth, window.innerHeight])
     }
 
     if (typeof window !== 'undefined') {
       handleResize()
       window.addEventListener('resize', handleResize)
-      
+
       return () => {
         window.removeEventListener('resize', handleResize)
       }
