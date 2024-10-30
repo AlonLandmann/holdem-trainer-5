18f4131d31b38355b3c702d3d5e9828c143adc30
AlonLandmann <rafael.landmann@gmail.com>
2024-10-30 16:55:16 +0800

    make hero section responsive

--- components/home/Hero.js
+++ components/home/Hero.js
@@ -4,7 +4,7 @@ import Button from '../_ui/Button'
 export default function Hero() {
   return (
     <div
-      className='relative flex justify-center items-center border-b'
+      className='relative p-7 flex justify-center items-center border-b'
       style={{ minHeight: 'calc(100vh - 56px)' }}
     >
       <div className='absolute top-0 left-0 z-10 h-full w-full bg-neutral-900 opacity-15'>
@@ -16,14 +16,14 @@ export default function Hero() {
           objectFit='cover'
         />
       </div>
-      <div className='max-w-[80%] flex flex-col items-center text-center z-20'>
-        <h1 className='mb-5 m-w-[480px] text-3xl font-medium text-neutral-50 md:text-4xl'>
+      <div className='flex flex-col items-start xs:items-center xs:text-center z-20'>
+        <h1 className='mb-5 max-w-[480px] text-3xl font-medium text-neutral-50 md:text-4xl'>
           Refine your game
         </h1>
         <h3 className='mb-7 max-w-[480px] text-neutral-300 md:text-lg'>
           Create your own custom poker ranges and train them on Hold'em Trainer.
         </h3>
-        <div className='flex gap-3'>
+        <div className='flex flex-col xs:flex-row gap-3'>
           <Button
             theme='nice'
             utilClasses='rounded-sm py-3 px-4 bg-opacity-80'
