66f253074fb00e735c34083a66b1c74c76a667da
AlonLandmann <rafael.landmann@gmail.com>
2024-10-31 10:19:48 +0800

    make contact section responsive

--- components/home/Contact.js
+++ components/home/Contact.js
@@ -2,11 +2,11 @@ import A from '../_ui/A'
 
 export default function Contact() {
   return (
-    <section className='flex flex-col items-center py-32 px-20 bg-neutral-800 bg-opacity-10 border-t border-b'>
+    <section className='flex flex-col items-center py-20 xl:py-32 px-12 xl:px-20 bg-neutral-800 bg-opacity-10 border-t border-b'>
       <h1 className='text-4xl mb-2'>
         Reach out
       </h1>
-      <h2 className='text-lg text-neutral-500 mb-8'>
+      <h2 className='text-lg text-neutral-500 mb-8 text-center'>
         We are always pleased to here from you.
       </h2>
       <A
@@ -14,7 +14,7 @@ export default function Contact() {
         text='info@holdem-trainer.com'
         utilClasses='text-xl mb-16'
       />
-      <div className='flex gap-16 text-6xl'>
+      <div className='flex flex-col xs:flex-row gap-16 text-6xl'>
         <A href='/' icon='youtube' />
         <A href='/' icon='twitter-x' />
         <A href='/' icon='discord' />
