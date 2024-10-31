e778a9581b43e12aeee9e6aea2c2e408612575e5
AlonLandmann <rafael.landmann@gmail.com>
2024-10-31 23:08:05 +0800

    attempt at fixing to reversed errors

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,65 @@
 import Page from '@/components/_layout/Page'
-import Link from 'next/link'
+import HomeRoot from '@/components/home/HomeRoot'
+import prisma from '@/lib/prisma'
+import { toClientFormat } from '@/lib/ranges'
 
-export default function HomePage() {
+export default function HomePage({ ranges, usageInfo, articles }) {
   return (
     <Page title="Hold'em Trainer">
-      <div className='bg-neutral-900 min-h-screen flex justify-center items-center'>
-        <div className='max-w-[500px] text-lg'>
-          We are currently in the process of updating the website. The new version should be live within 24 hours.
-          You can try to access the service directly by navigating to <Link href='/auth/signup'>https://www.holdem-trainer/auth/signup</Link>.
-          Otherwise you can reach out directly to us at info@holdem-trainer.com for a prompt response. Please bear with us.
-        </div>
-      </div>
+      <HomeRoot
+        ranges={JSON.parse(ranges)}
+        usageInfo={JSON.parse(usageInfo)}
+        articles={JSON.parse(articles)}
+      />
     </Page>
   )
+}
+
+export async function getServerSideProps() {
+  const ranges = await prisma.range.findMany({
+    where: {
+      folder: {
+        is: {
+          userId: Number(process.env.SAMPLE_USER_ID),
+        }
+      }
+    }
+  })
+
+  const nrUsers = await prisma.user.count({})
+  const nrRanges = await prisma.range.count({})
+  const nrCombos = await prisma.trainingUnit.aggregate({
+    _sum: {
+      total: true,
+    },
+  })
+
+  const articles = await prisma.article.findMany({
+    where: {
+      isPublished: true,
+    },
+    select: {
+      id: true,
+      publishedAt: true,
+      imageUrl: true,
+      readTime: true,
+      level: true,
+      title: true,
+      slug: true,
+      abstract: true,
+    },
+    orderBy: {
+      id: 'asc'
+    },
+    skip: 1,
+    take: 3,
+  })
+
+  return {
+    props: {
+      ranges: JSON.stringify(ranges.map(r => toClientFormat(r))),
+      usageInfo: JSON.stringify({ nrUsers, nrRanges, nrCombos: nrCombos._sum.total }),
+      articles: JSON.stringify(articles)
+    }
+  }
 }
