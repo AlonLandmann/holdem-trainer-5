2ec80831f450f135cc7c3fd366800e9f050238e1
AlonLandmann <rafael.landmann@gmail.com>
2024-11-08 00:46:33 +0800

    add rest of state to solver

--- components/solver/SolverMain.js
+++ components/solver/SolverMain.js
@@ -3,15 +3,17 @@ import FrequencySetting from './FrequencySetting'
 import BoardSetting from './BoardSetting'
 
 export default function SolverMain() {
-  const [board, setBoard] = useState(['Ac', 'Qs', 'Jd', '5h', '8d'])
-  const [frequencies, setFrequencies] = useState([
-    Array(1326).fill(1000),
-    Array(1326).fill(1000),
-    Array(1326).fill(1000),
-    Array(1326).fill(1000),
-    Array(1326).fill(1000),
-    Array(1326).fill(1000)
-  ])
+  const [street, setStreet] = useState(3)
+  const [board, setBoard] = useState(['As', 'Qs', 'Jd', '5h', '8d'])
+  const [frequencies, setFrequencies] = useState(Array(6).fill(Array(1326).fill(1000)))
+  const [player, setPlayer] = useState(0)
+  const [bigBlind, setBigBlind] = useState(2)
+  const [minRaise, setMinRaise] = useState(2)
+  const [hasFolded, setHasFolded] = useState([false, false, true, true, true, true])
+  const [hasActed, setHasActed] = useState([false, false, false, false, false, false])
+  const [stacks, setStacks] = useState([3, 3, 4, 4, 4, 4])
+  const [committed, setCommitted] = useState([0, 0, 0, 0, 0, 0])
+  const [mainPotShares, setMainPotShares] = useState([0, 0, 0, 0, 0, 0])
 
   return (
     <div className='grow overflow-x-auto'>
@@ -37,35 +39,3 @@ export default function SolverMain() {
     </div>
   )
 }
-
-// int board[5] = {
-//   create_card('Q', 's'),
-//   create_card('7', 'd'),
-//   create_card('2', 's'),
-//   create_card('6', 'd'),
-//   create_card('3', 'd')
-// };
-
-// int big_blind = 2;
-// node start_node = {
-//   .has_folded = {0, 0, 1, 1, 1, 1},
-//   .has_acted = {0, 0, 0, 0, 0, 0},
-//   .stacks = {3, 3, 5, 5, 5, 5},
-//   .committed = {0, 0, 0, 0, 0, 0},
-//   .main_pot_shares = {2, 2, 0, 0, 0, 0},
-//   .min_raise = 2,
-//   .street = 3,
-//   .player = 0
-// };
-
-// DONE
-
-// int nr_combos_per_player[6] = {3, 3, 0, 0, 0, 0};
-// int frequencies[18] = {
-//   create_card('A', 's'), create_card('A', 'd'), 100,
-//   create_card('K', 's'), create_card('K', 'd'), 100,
-//   create_card('Q', 'h'), create_card('Q', 'd'), 100,
-//   create_card('A', 's'), create_card('A', 'd'), 100,
-//   create_card('K', 's'), create_card('K', 'd'), 100,
-//   create_card('Q', 'h'), create_card('Q', 'd'), 100
-// };
