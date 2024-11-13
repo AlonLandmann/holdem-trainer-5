8ed10f27a6ce89689a94594eb029524cd0f5d990
AlonLandmann <rafael.landmann@gmail.com>
2024-11-13 16:45:36 +0800

    add sample ranges for new users

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -4,6 +4,7 @@ import prisma from '@/lib/prisma'
 import { generateUsername } from '@/lib/usernames'
 import { last } from 'lodash'
 import sha256 from 'sha256'
+import sampleFolders from '../../../prisma/sample-ranges.json'
 
 export default async function handler(req, res) {
   try {
@@ -34,6 +35,22 @@ export default async function handler(req, res) {
           userNameCandidate = `${userNameCandidate} ${String(last(conflictingEndings) + 1)}`
         }
 
+        const folderCreateArray = sampleFolders.map(folder => ({
+          index: folder.index,
+          name: `${folder.name} (sample)`,
+          ranges: {
+            create: folder.ranges.map(range => ({
+              index: range.index,
+              name: range.name,
+              stacks: range.stacks,
+              history: range.history,
+              options: range.options,
+              matrix: Buffer.from(range.matrix),
+              complexity: range.complexity,
+            }))
+          }
+        }))
+
         const newUser = await prisma.user.create({
           data: {
             email,
@@ -41,11 +58,60 @@ export default async function handler(req, res) {
             hash: sha256(password + process.env.PASSWORD_SALT),
             session: { create: {} },
             settings: { create: {} },
+            folders: { create: folderCreateArray },
           },
-          include: {
-            session: true
+          select: {
+            id: true,
+            session: {
+              select: {
+                token: true,
+              }
+            }
           }
         })
+
+        const predecessorConnections = {
+          'UTG vs HJ 3-bet': 'UTG Open',
+          'UTG vs CO 3-bet': 'UTG Open',
+          'UTG vs BTN 3-bet': 'UTG Open',
+          'UTG vs SB 3-bet': 'UTG Open',
+          'UTG vs BB 3-bet': 'UTG Open',
+          'HJ vs CO 3-bet': 'HJ Open',
+          'HJ vs BTN 3-bet': 'HJ Open',
+          'HJ vs SB 3-bet': 'HJ Open',
+          'HJ vs BB 3-bet': 'HJ Open',
+          'CO vs BTN 3-bet': 'CO Open',
+          'CO vs SB 3-bet': 'CO Open',
+          'CO vs BB 3-bet': 'CO Open',
+          'BTN vs SB 3-bet': 'BTN Open',
+          'BTN vs BB 3-bet': 'BTN Open',
+        }
+
+        for (let successor in predecessorConnections) {
+          const predecessor = await prisma.range.findFirst({
+            where: {
+              folder: {
+                userId: newUser.id
+              },
+              name: predecessorConnections[successor]
+            },
+            select: {
+              id: true
+            }
+          })
+
+          await prisma.range.updateMany({
+            where: {
+              folder: {
+                userId: newUser.id
+              },
+              name: successor
+            },
+            data: {
+              predecessorId: predecessor.id
+            }
+          })
+        }
 
         setSessionCookie(res, newUser.session.token)
         sendVerificationLink(newUser.email, newUser.verificationToken)
