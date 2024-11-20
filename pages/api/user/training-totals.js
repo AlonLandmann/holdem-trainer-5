f1c635842763df6fa71077b690e061ae2c5b7809
AlonLandmann <rafael.landmann@gmail.com>
2024-11-20 15:13:23 +0800

    FIX overview crush due to null training totals

--- pages/api/user/training-totals.js
+++ pages/api/user/training-totals.js
@@ -25,6 +25,18 @@ export default async function handler(req, res) {
           return res.status(200).json({ success: false, message: 'Training totals not found.' });
         }
 
+        if (!trainingTotals._sum.correct) {
+          trainingTotals._sum.correct = 0;
+        }
+
+        if (!trainingTotals._sum.total) {
+          trainingTotals._sum.total = 0;
+        }
+
+        if (!trainingTotals._sum.score) {
+          trainingTotals._sum.score = 0;
+        }
+
         return res.status(200).json({ success: true, trainingTotals: trainingTotals._sum });
       default:
         return res.status(400).json({ success: false, message: 'Invalid request.' });
