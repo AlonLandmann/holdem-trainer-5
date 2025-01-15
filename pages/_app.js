0a8999250dbda108b1592243ac47b6af4dbf80f2
AlonLandmann <rafael.landmann@gmail.com>
2025-01-15 19:08:53 +0800

    FIX crash due to useUser context remaining

--- pages/_app.js
+++ pages/_app.js
@@ -1,31 +1,28 @@
-import { LoadingQueueProvider } from '@/hooks/useLoadingQueue'
-import { UserProvider } from '@/hooks/useUser'
-import '@/styles/globals.css'
-import { Toaster } from 'react-hot-toast'
-import PlausibleProvider from 'next-plausible'
-import { UserDataProvider } from '@/hooks/useUserData'
+import { LoadingQueueProvider } from "@/hooks/useLoadingQueue";
+import { Toaster } from "react-hot-toast";
+import PlausibleProvider from "next-plausible";
+import { UserDataProvider } from "@/hooks/useUserData";
+import "@/styles/globals.css";
 
 
 export default function App({ Component, pageProps }) {
-  return (
-    <PlausibleProvider domain='holdem-trainer.com'>
-      <LoadingQueueProvider>
-        <UserProvider>
-          <UserDataProvider>
-            <Component {...pageProps} />
-            <Toaster
-              toastOptions={{
-                style: {
-                  border: '1px solid rgb(38, 38, 38)',
-                  color: 'rgb(229, 229, 229)',
-                  backgroundColor: 'rgb(23, 23, 23)',
-                  opacity: 0.8,
-                }
-              }}
-            />
-          </UserDataProvider>
-        </UserProvider>
-      </LoadingQueueProvider>
-    </PlausibleProvider>
-  )
-}
+    return (
+        <PlausibleProvider domain="holdem-trainer.com">
+            <LoadingQueueProvider>
+                <UserDataProvider>
+                    <Component {...pageProps} />
+                    <Toaster
+                        toastOptions={{
+                            style: {
+                                border: "1px solid rgb(38, 38, 38)",
+                                color: "rgb(229, 229, 229)",
+                                backgroundColor: "rgb(23, 23, 23)",
+                                opacity: 0.8,
+                            }
+                        }}
+                    />
+                </UserDataProvider>
+            </LoadingQueueProvider>
+        </PlausibleProvider>
+    );
+};
