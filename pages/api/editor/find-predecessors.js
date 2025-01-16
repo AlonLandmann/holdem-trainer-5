ab35140e2ceef557345712a40f843805242f33a5
AlonLandmann <rafael.landmann@gmail.com>
2025-01-16 10:31:35 +0800

    refactor editor api routes

--- pages/api/editor/find-predecessors.js
+++ pages/api/editor/find-predecessors.js
@@ -2,63 +2,50 @@ import prisma from "@/lib/prisma";
 import { actionToStorage, optionToStorage, toClientFormat } from "@/lib/ranges";
 import messages from "@/lib/messages";
 
-// not secured, but it's just a GET method
-
 export default async function handler(req, res) {
-  if (req.method !== "GET") {
-    return res.status(405).json({ success: false, message: messages.invalidRequestMethod });
-  }
+    if (req.method !== "GET") {
+        return res.status(405).json({ success: false, message: messages.invalidRequestMethod });
+    }
 
-  if (
-    !req.query.userId ||
-    !req.query.history ||
-    !req.query.option
-  ) {
-    return res.status(400).json({ success: false, message: messages.missingQueryData });
-  }
+    if (!req.query.userId || !req.query.history || !req.query.option) {
+        return res.status(400).json({ success: false, message: messages.missingQueryData });
+    }
 
-  let historyToMatch;
+    let historyToMatch;
+    let optionToMatch;
 
-  try {
-    historyToMatch = JSON.parse(req.query.history);
-  } catch (error) {
-    console.log(error);
-    return res.status(400).json({ success: false, message: messages.corruptedInput });
-  }
+    try {
+        historyToMatch = JSON.parse(req.query.history);
+        optionToMatch = JSON.parse(req.query.option);
+    } catch (error) {
+        console.log(error);
+        return res.status(400).json({ success: false, message: messages.corruptedInput });
+    }
 
-  let optionToMatch;
+    let ranges;
 
-  try {
-    optionToMatch = JSON.parse(req.query.option);
-  } catch (error) {
-    console.log(error);
-    return res.status(400).json({ success: false, message: messages.corruptedInput });
-  }
+    try {
+        ranges = await prisma.range.findMany({
+            where: {
+                folder: {
+                    is: {
+                        userId: Number(req.query.userId),
+                    },
+                },
+                history: {
+                    equals: historyToMatch.map(a => actionToStorage(a)),
+                },
+                options: {
+                    has: optionToStorage(optionToMatch),
+                },
+            },
+        });
 
-  let ranges;
+        ranges = ranges.map(r => toClientFormat(r));
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ success: false, message: messages.internalServerError });
+    }
 
-  try {
-    ranges = await prisma.range.findMany({
-      where: {
-        folder: {
-          is: {
-            userId: Number(req.query.userId),
-          },
-        },
-        history: {
-          equals: historyToMatch.map(a => actionToStorage(a)),
-        },
-        options: {
-          has: optionToStorage(optionToMatch),
-        },
-      },
-    });
-
-    ranges = ranges.map(r => toClientFormat(r));
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({ success: false, message: messages.internalServerError });
-  }
-
-  return res.status(200).json({ success: true, ranges: ranges });
+    return res.status(200).json({ success: true, ranges: ranges });
 };
--- pages/api/editor/save-changes.js
+++ pages/api/editor/save-changes.js
@@ -3,68 +3,66 @@ import { toStorageFormat } from "@/lib/ranges";
 import { computeComplexity } from "@/lib/stats";
 import messages from "@/lib/messages";
 
-// Still need to separate errors (could be unauthorized error in the second call)
-
 export default async function (req, res) {
-  if (req.method !== "PUT") {
-    return res.status(405).json({ success: false, message: messages.invalidRequestMethod });
-  }
+    if (req.method !== "PUT") {
+        return res.status(405).json({ success: false, message: messages.invalidRequestMethod });
+    }
 
-  if (!req.cookies.holdemTrainerSessionId) {
-    return res.status(401).json({ success: false, message: messages.noSessionDetected });
-  }
+    if (!req.cookies.holdemTrainerSessionId) {
+        return res.status(401).json({ success: false, message: messages.noSessionDetected });
+    }
 
-  let user;
+    let user;
 
-  try {
-    user = await prisma.user.findUnique({
-      where: {
-        sessionCookie: req.cookies.holdemTrainerSessionId,
-      },
-    });
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({ success: false, message: messages.internalServerError });
-  }
+    try {
+        user = await prisma.user.findUnique({
+            where: {
+                sessionCookie: req.cookies.holdemTrainerSessionId,
+            },
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ success: false, message: messages.internalServerError });
+    }
 
-  if (!user) {
-    return res.status(401).json({ success: false, message: messages.userNotFound });
-  }
+    if (!user) {
+        return res.status(401).json({ success: false, message: messages.userNotFound });
+    }
 
-  let range;
+    let range;
 
-  try {
-    range = req.body;
-    range.complexity = computeComplexity(range);
-    range = toStorageFormat(range);
-  } catch (error) {
-    console.log(error)
-    return res.status(400).json({ success: false, message: messages.corruptedInput });
-  }
+    try {
+        range = req.body;
+        range.complexity = computeComplexity(range);
+        range = toStorageFormat(range);
+    } catch (error) {
+        console.log(error)
+        return res.status(400).json({ success: false, message: messages.corruptedInput });
+    }
 
-  try {
-    await prisma.range.update({
-      where: {
-        id: range.id,
-        folder: {
-          is: {
-            userId: user.id,
-          },
-        },
-      },
-      data: {
-        stacks: range.stacks,
-        history: range.history,
-        options: range.options,
-        matrix: range.matrix,
-        complexity: range.complexity,
-        predecessorId: range.predecessorId,
-      },
-    });
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({ success: false, message: messages.internalServerError }); 
-  }
+    try {
+        await prisma.range.update({
+            where: {
+                id: range.id,
+                folder: {
+                    is: {
+                        userId: user.id,
+                    },
+                },
+            },
+            data: {
+                stacks: range.stacks,
+                history: range.history,
+                options: range.options,
+                matrix: range.matrix,
+                complexity: range.complexity,
+                predecessorId: range.predecessorId,
+            },
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ success: false, message: messages.internalServerError });
+    }
 
-  return res.status(200).json({ success: true });
+    return res.status(200).json({ success: true });
 };
